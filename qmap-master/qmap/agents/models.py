c7b6f84b88fb1bdcf7899bd598f8e6551b70104d
darakshan <darakshan@tamu.edu>
2020-04-28 03:41:13 -0500

    Added

--- qmap-master/qmap/agents/models.py
+++ qmap-master/qmap/agents/models.py
@@ -47,7 +47,7 @@ def ConvMlp(convs, hiddens, dueling=False, layer_norm=False):
 
 
 class ConvDeconvMap(object):
-    def __init__(self, convs, middle_hiddens, deconvs, coords_shape, dueling, layer_norm, activation_fn):
+    def __init__(self, convs, middle_hiddens, deconvs, coords_shape, dueling=True, layer_norm=True, activation_fn=tf.nn.elu):
         self.description = 'ConvDeconvMap-' + str(convs + middle_hiddens + deconvs) + '-' + activation_fn.__repr__().split(' ')[1]
         self.description = self.description.replace(' ', '')
         if dueling: self.description += '-duel'
