0baef66a67ce17973f39d6307c0cf0d8bab9ba89
darakshan <darakshan@tamu.edu>
2020-04-28 22:01:05 -0500

    change_directory

--- qmap-master/qmap/train_montezuma.py
+++ qmap-master/qmap/train_montezuma.py
@@ -4,14 +4,11 @@ from baselines.common.misc_util import boolean_flag
 from baselines.common.schedules import LinearSchedule
 import tensorflow as tf
 import time
-
 from qmap.models import ConvDeconvMap, ConvMlp
 from qmap.q_map_dqn_agent import Q_Map_DQN_Agent
 from qmap.replay_buffers import DoublePrioritizedReplayBuffer
 from qmap.custom_montezuma import CustomMontezumaEnv
 from qmap.wrappers import PerfLogger
-
-
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--seed', help='RNG seed', type=int, default=0)
 parser.add_argument('--path', default='qmap_results')
@@ -21,32 +18,23 @@ parser.add_argument('--model_save_freq', help='model save frequncy', type=int, d
 boolean_flag(parser, 'dqn', default=True)
 boolean_flag(parser, 'qmap', default=True)
 boolean_flag(parser, 'render', help='play the videos', default=False)
+seed=0
+path="qmap_results"
+n_steps=int(1000)
+save_freq=10000
 args = parser.parse_args()
 
-# n_steps = int(5e6)
-n_steps = int(args.n_steps)
 env = CustomMontezumaEnv(screen_ratio=4, coords_ratio=8, use_color=False, use_rc_frame=False, stack=3, frame_skip=2, action_repeat=4)
 coords_shape = env.coords_shape
-set_global_seeds(args.seed)
-env.seed(args.seed)
+set_global_seeds(seed)
+env.seed(seed)
 task_gamma = 0.99
-
-print('~~~~~~~~~~~~~~~~~~~~~~')
-print(env)
-print(env.unwrapped.name)
-print('observations:', env.observation_space.shape)
-print('coords:     ', coords_shape)
-print('actions:    ', env.action_space.n)
-print('~~~~~~~~~~~~~~~~~~~~~~')
-
 config = tf.ConfigProto(allow_soft_placement=True)
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 sess.__enter__()
-
-# Q-map
-if args.qmap:
-    q_map_model = ConvDeconvMap(
+############### comment if not qmap #############
+q_map_model = ConvDeconvMap(
         convs=[(32, 8, 2), (32, 6, 2), (64, 4, 2)],
         middle_hiddens=[1024],
         deconvs=[(64, 4, 2), (32, 6, 2), (env.action_space.n, 4, 1)],
@@ -55,30 +43,17 @@ if args.qmap:
         layer_norm=True,
         activation_fn=tf.nn.elu
     )
-    q_map_random_schedule = LinearSchedule(schedule_timesteps=n_steps, initial_p=0.1, final_p=0.05)
-else:
-    q_map_model = None
-    q_map_random_schedule = None
-
-# DQN
-if args.dqn:
-    dqn_model = ConvMlp(
+q_map_random_schedule = LinearSchedule(schedule_timesteps=n_steps, initial_p=0.1, final_p=0.05)
+###################################################
+############# Comment if not dqn agent ###############
+dqn_model = ConvMlp(
         convs=[(32, 8, 2), (32, 6, 2), (32, 4, 2)],
         hiddens=[1024],
         dueling=True,
     )
     exploration_schedule = LinearSchedule(schedule_timesteps=n_steps, initial_p=1.0, final_p=0.05)
-else:
-    dqn_model = None
-    exploration_schedule = LinearSchedule(schedule_timesteps=n_steps, initial_p=1.0, final_p=1.0)
-
-
-if q_map_model is not None or dqn_model is not None:
-    double_replay_buffer = DoublePrioritizedReplayBuffer(int(5e5), alpha=0.6, epsilon=1e-6, timesteps=n_steps, initial_p=0.4, final_p=1.0)
-else:
-    double_replay_buffer = None
-
-
+########################################################
+double_replay_buffer = DoublePrioritizedReplayBuffer(int(5e5), alpha=0.6, epsilon=1e-6, timesteps=n_steps, initial_p=0.4, final_p=1.0)
 agent_name = '{}M'.format(n_steps//int(1e6))
 agent = Q_Map_DQN_Agent(
     observation_space=env.observation_space,
@@ -87,14 +62,14 @@ agent = Q_Map_DQN_Agent(
     double_replay_buffer=double_replay_buffer,
     task_gamma=task_gamma,
     exploration_schedule=exploration_schedule,
-    seed=args.seed,
-    path=args.path,
+    seed=seed,
+    path=path,
     learning_starts=1000,
     train_freq=4,
     print_freq=1,
     env_name=env.unwrapped.name,
     agent_name=agent_name,
-    renderer_viewer=args.render,
+    renderer_viewer=False,
     # DQN
     dqn_q_func=dqn_model,
     dqn_lr=1e-4,
@@ -102,12 +77,12 @@ agent = Q_Map_DQN_Agent(
     dqn_batch_size=32,
     dqn_target_net_update_freq=1000,
     dqn_grad_norm_clip=1000,
-    dqn_model_save_freq=args.model_save_freq,
+    dqn_model_save_freq=save_freq,
     # Q-Map
     q_map_model=q_map_model,
     q_map_random_schedule=q_map_random_schedule,
     q_map_greedy_bias=0.5,
-    q_map_timer_bonus=0.5, # 50% more time than predicted
+    q_map_timer_bonus=0.5,
     q_map_lr=3e-4,
     q_map_gamma=0.9,
     q_map_n_steps=1,
@@ -118,12 +93,7 @@ agent = Q_Map_DQN_Agent(
     q_map_max_goal_steps=30,
     q_map_grad_norm_clip=1000
 )
-if args.load is not None:
-    agent.load(args.load)
-agent.seed(args.seed)
-
 env = PerfLogger(env, agent.task_gamma, agent.path)
-
 done = True
 episode = 0
 score = None
